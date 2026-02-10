fbd234865887819c828bfee15a835ef3ce643ac6
mkaufman2023 <mkaufman2023@gmail.com>
2026-02-09 19:10:19 -0600

    init enteliscript for testing

--- enteliscript.py
+++ enteliscript.py
@@ -3,3 +3,9 @@ enteliscript.py
 
 CLI for enteliweb.
 """
+from enteliweb import EnteliWEB
+
+
+
+if __name__ == "__main__":
+    api = EnteliWEB(username="admin", password="password", server_ip="192.168.1.100", site_name="DefaultSite")
